70046f0a04bf674e5c480dcde839773114289914
Ritika Raj <rajritika999@gmail.com>
2026-03-02 10:03:40 +0530

    Two sum code

--- Arrays/TwoSum.java
+++ Arrays/TwoSum.java
@@ -1,1 +1,27 @@
+package Arrays;
 
+// Write a class Solution with a method twoSum that takes an array of integers and a target integer as input, and returns the indices of the two numbers in the array that add up to the target. You can assume that there is exactly one solution, and you may not use the same element twice.
+
+import java.util.HashMap;
+
+public class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[] { map.get(complement), i };
+            }
+            map.put(nums[i], i);
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+
+    public static void main(String[] args) {
+        TwoSum solution = new TwoSum();
+        int[] nums = {2, 11, 7, 15};
+        int target = 9;
+        int[] result = solution.twoSum(nums, target);
+        System.out.println("Indices: " + result[0] + ", " + result[1]);
+    }
+}
